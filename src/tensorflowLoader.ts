74ac2df284282c0ee139b86e26d27d1be393c742
Jørgen Tvedt <tvedtorama@gmail.com>
2023-01-19 15:54:59 +0100

    Fixed missing gpu reference

--- src/tensorflowLoader.ts
+++ src/tensorflowLoader.ts
@@ -1,11 +1,15 @@
 import {Tensor, SymbolicTensor, LayersModel} from "@tensorflow/tfjs-node"
 // import {Layer}  from "@tensorflow/tfjs-layers/dist/engine"
 
-export {Tensor, SymbolicTensor, LayersModel}
+/* type Tensor = any
+type SymbolicTensor = any
+type LayersModel = any */
+
+export type {Tensor, SymbolicTensor, LayersModel}
 
 export const loadTfjsNode = () => import("@tensorflow/tfjs-node")
 
 const loadTfjs = loadTfjsNode
-export const loadTfjsGpu = () => import("@tensorflow/tfjs-node")
+export const loadTfjsGpu = () => import("@tensorflow/tfjs-node-gpu")
 
 export type ITensorflow = Awaited<ReturnType<typeof loadTfjs>>
