0b547743b8d173ff41b90617ca067da48cf42c2c
Jørgen Tvedt <tvedtorama@gmail.com>
2023-01-19 10:55:17 +0100

    Formatting

--- src/train.ts
+++ src/train.ts
@@ -1,29 +1,26 @@
 import tf from "@tensorflow/tfjs-node"
-import { map, Observable } from "rxjs"
-import { autoEncoderLoss, imageDim, setupAutoEncoder, setupDecoder, setupEncoder } from "./model.js"
+import {map, Observable} from "rxjs"
+import {autoEncoderLoss, imageDim, setupAutoEncoder, setupDecoder, setupEncoder} from "./model.js"
 
-export const train = async (chunks: Observable<{buffer: number[], length: number}>) => {
-
+export const train = async (chunks: Observable<{buffer: number[]; length: number}>) => {
 	const encoder = setupEncoder()
 	const decoder = setupDecoder()
 	const autoEncoder = setupAutoEncoder(encoder, decoder)
 
-	const optimizer = tf.train.adam();
+	const optimizer = tf.train.adam()
 
-	await chunks.pipe(
-		map(({buffer, length}, idx) => ({tensor: tf.tensor4d(buffer, [length, ...imageDim]), idx}))
-	).forEach(({tensor, idx}) => {
+	await chunks.pipe(map(({buffer, length}, idx) => ({tensor: tf.tensor4d(buffer, [length, ...imageDim]), idx}))).forEach(({tensor, idx}) => {
 		optimizer.minimize(() => {
 			const outputs = autoEncoder.apply(tensor)
 			const loss = autoEncoderLoss(tensor, <any>outputs)
 
-			process.stdout.write('.');
+			process.stdout.write(".")
 			if (idx % 50 === 0) {
-				console.log('\nLoss:', loss.dataSync()[0]);
+				console.log("\nLoss:", loss.dataSync()[0])
 			}
 
 			return <tf.Scalar>loss
 		})
-		tf.dispose([tensor]);
+		tf.dispose([tensor])
 	})
 }
