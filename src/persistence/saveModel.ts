09ba1e53e9041d90b47ace6c297c1a2e5f4e1eaa
Jørgen Tvedt <tvedtorama@gmail.com>
2023-01-19 15:48:24 +0100

    Reintroduced mkdirp

--- src/persistence/saveModel.ts
+++ src/persistence/saveModel.ts
@@ -1,8 +1,10 @@
 import {LayersModel} from "@tensorflow/tfjs-node"
 import path from "path"
+import mkdirp from "mkdirp"
 
 export const saveModel = async (model: LayersModel, savePath: string, title = "decoder") => {
 	const decoderPath = path.join(savePath, title)
+	mkdirp.sync(decoderPath)
 	const saveURL = `file://${decoderPath}`
 	console.log(`Saving decoder to ${saveURL}`)
 	await model.save(saveURL)
