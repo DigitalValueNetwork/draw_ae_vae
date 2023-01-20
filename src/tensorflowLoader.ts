1cc911f599bab34aa97d457a496e15a7ff4dd4a6
Jørgen Tvedt <tvedtorama@gmail.com>
2023-01-20 07:34:56 +0100

    Comments and tidying

--- src/tensorflowLoader.ts
+++ src/tensorflowLoader.ts
@@ -1,16 +1,11 @@
 // Huge issues with tensorflow typing when introducing tfjs-node-gpu
 import {Tensor, SymbolicTensor, LayersModel} from "@tensorflow/tfjs-node"
-// import {Layer}  from "@tensorflow/tfjs-layers/dist/engine"
 
-/* type Tensor = any
-type SymbolicTensor = any
-type LayersModel = any */
-
+// Exporting these types with the `type` keyword might make a difference in what content is actually imported above. 
 export type {Tensor, SymbolicTensor, LayersModel}
 
 export const loadTfjsNode = () => import("@tensorflow/tfjs-node")
-
-const loadTfjs = loadTfjsNode
 export const loadTfjsGpu = () => import("@tensorflow/tfjs-node-gpu")
 
+const loadTfjs = loadTfjsNode
 export type ITensorflow = Awaited<ReturnType<typeof loadTfjs>>
