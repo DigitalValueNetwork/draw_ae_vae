17edf3697e18b3088ed92b9e2d811107b604be1f
Jørgen Tvedt <tvedtorama@gmail.com>
2023-03-23 15:24:28 +0100

    Note about shape

--- src/model.ts
+++ src/model.ts
@@ -93,7 +93,7 @@ export function autoEncoderLoss(inputs: Tensor, outputs: Tensor[], tf: ITensorfl
 
 		// First we compute a 'reconstruction loss' terms. The goal of minimizing
 		// this term is to make the model outputs match the input data.
-		const reconstructionLoss = tf.losses.meanSquaredError(inputs, decoderOutput).mul(originalDim * originalDim)
+		const reconstructionLoss = tf.losses.meanSquaredError(inputs, decoderOutput).mul(originalDim * originalDim) // shape: 1, not [1] (?)
 
 		// binaryCrossEntropy can be used as an alternative loss function
 		// const reconstructionLoss =
